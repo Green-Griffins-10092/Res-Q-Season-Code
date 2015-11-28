bcf42f61ed6d4240c07367862ee4b7f8e4e839a9
David <dflores0818@gmail.com>
2015-11-27 16:23:18 -0800

    continued working on MotorServoTest class.   Motors not in use will float. Added a list of todo items.  Added some comments to the RobotHardware class.   Removed lines from the opmode register.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Green/Griffins/MotorServoTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Green/Griffins/MotorServoTest.java
@@ -26,10 +26,35 @@ public class MotorServoTest extends OpMode{
         } catch (Exception e) {
             telemetry.addData("Warning", "No test motor!");
         }
+
+        for (DcMotor motor :
+                hardwareMap.dcMotor) {
+            motor.setPowerFloat();
+        }
+
+        //servos do not have a float method
     }
 
     @Override
     public void loop() {
+        //TODO: have a joystick control direction of servo
+        //TODO: have a button that switches the direction of the servo and motor
+        //TODO: have controls that precisely change the servo value (motor too?)
+        //TODO: reset encoder value button? then also a button that moves motor very precisely, and changes motor mode
 
+        if (testMotor != null){
+            testMotor.setPower(-gamepad1.left_stick_y);
+
+            telemetry.addData("Motor power", testMotor.getPower());
+            telemetry.addData("Motor encoder position", testMotor.getCurrentPosition());
+            telemetry.addData("Motor direction", testMotor.getDirection());
+        }
+
+        if (testServo != null) {
+            testServo.setPosition(.33);
+            telemetry.addData("Servo position", testServo.getPosition());
+            telemetry.addData("Servo direction", testServo.getDirection());
+        }
     }
+
 }
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Green/Griffins/RobotHardware.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Green/Griffins/RobotHardware.java
@@ -8,23 +8,39 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Created by David F. on 11/10/2015.
  * To store the hardware for the robot
  */
-public class RobotHardware {
+public final class RobotHardware {
 
     public static final String[] HARDWARE_MOTOR_NAMES = {"left drive 1", "left drive 2", "right drive 1", "right drive 2", "left arm pivot", "right arm pivot", "spool"};
     public static final String[] HARDWARE_SERVO_NAMES = {"left shifter servo", "right shifter servo", "ratchet servo"};
     public static final String[] HARDWARE_SENSOR_NAMES = {};
 
+    //shifter variables
     private Shifter leftDrive;
     private Shifter rightDrive;
+
+    //motor variables
     private DcMotor spoolMotor;
     private DcMotor leftArmPivot;
     private DcMotor rightArmPivot;
 
+    //servo variables
     private Servo spoolLockServo;
 
-    public static RobotHardware initialize(HardwareMap hardwareMap) {
-        RobotHardware hardware = new RobotHardware();
+    private RobotHardware()
+    {}
 
+    /**
+     * This method creates and fills a RobotHardware object
+     * This is the only way to create a RobotHardware object outside of RobotHardware
+     * @param hardwareMap
+     * A hardware map, which must contain the hardware names for the hardware
+     * @return
+     * A fully populated HardwareObject
+     */
+    public static RobotHardware initialize(HardwareMap hardwareMap) {
+        RobotHardware hardware = new RobotHardware();  //initialize the object
+
+        //assign all the fields to their values
         hardware.leftDrive = new Shifter(hardwareMap.dcMotor.get(HARDWARE_MOTOR_NAMES[0]), hardwareMap.dcMotor.get(HARDWARE_MOTOR_NAMES[1]),
                 hardwareMap.servo.get(HARDWARE_SERVO_NAMES[0]));
         hardware.rightDrive = new Shifter(hardwareMap.dcMotor.get(HARDWARE_MOTOR_NAMES[2]), hardwareMap.dcMotor.get(HARDWARE_MOTOR_NAMES[3]),
@@ -42,6 +58,6 @@ public class RobotHardware {
 
         hardware.spoolLockServo = hardwareMap.servo.get(HARDWARE_SERVO_NAMES[2]);
 
-        return hardware;
+        return hardware; //return the object
     }
 }
