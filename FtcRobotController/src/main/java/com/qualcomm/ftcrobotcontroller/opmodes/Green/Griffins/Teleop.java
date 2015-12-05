6fe65c736109c7a7a1e3b7118dad323ca41dfeb3
David <dflores0818@gmail.com>
2015-12-04 18:02:42 -0800

    Added shifter power and arm intake power to sendTelemetryData. Added a variable called shifting in loop to ensure that the shifter motors are not running while a shifting operation is in progress. Made necessary changes to code to use this variable. Added the code for the arm intake motor.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Green/Griffins/Teleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Green/Griffins/Teleop.java
@@ -20,12 +20,16 @@ public class Teleop extends OpMode {
         return input;
     }
 
-    private void sendTelemetryData()
-    {
+    private void sendTelemetryData() {
         telemetry.addData("00", locked ? "Controllers locked" : "Controllers unlocked");
+
+        telemetry.addData("01 left shifter power", hardware.getLeftDrive().getPower());
+        telemetry.addData("01 right shifter power", hardware.getRightDrive().getPower());
 
         telemetry.addData("02 left shifter position", hardware.getLeftDrive().getPosition());
         telemetry.addData("02 right shifter position", hardware.getRightDrive().getPosition());
+
+        telemetry.addData("05 arm intake power", hardware.getArmIntakeMotor().getPower());
 
         telemetry.addData("99 Gamepad 1", gamepad1.toString());
         telemetry.addData("99 Gamepad 2", gamepad2.toString());
@@ -40,6 +44,7 @@ public class Teleop extends OpMode {
 
         //TODO: set button pusher to center position
         locked = true;
+
     }
 
     @Override
@@ -54,21 +59,34 @@ public class Teleop extends OpMode {
         //control code
         if (!locked) {
 
-            // create a reference
+            // create a reference for the shifters
             Shifter leftShifter, rightShifter;
             leftShifter = hardware.getLeftDrive();
             rightShifter = hardware.getRightDrive();
 
+            /*
+            makes sure that motors are not drive while shifting if this is true,
+            a shifting operation is going to occur at the end of the loop,
+            and it is not safe to command the motors to move
+            */
+            boolean shifting = false;
+
             //shifting
-            if (gamepad1.left_bumper && gamepad1.left_trigger == 1) {
+            if (gamepad1.left_bumper && gamepad1.left_trigger == 1 &&
+                    leftShifter.getPosition() == Shifter.ShifterPosition.NEUTRAL) {
                 leftShifter.shift(Shifter.ShifterPosition.NEUTRAL);
                 rightShifter.shift(Shifter.ShifterPosition.NEUTRAL);
-            } else if (gamepad1.left_trigger == 1) {
+                shifting = true;
+            } else if (gamepad1.left_trigger == 1 &&
+                    leftShifter.getPosition() == Shifter.ShifterPosition.DRIVE) {
                 leftShifter.shift(Shifter.ShifterPosition.DRIVE);
                 rightShifter.shift(Shifter.ShifterPosition.DRIVE);
-            } else if (gamepad1.left_bumper) {
+                shifting = true;
+            } else if (gamepad1.left_bumper &&
+                    leftShifter.getPosition() == Shifter.ShifterPosition.ARM) {
                 leftShifter.shift(Shifter.ShifterPosition.ARM);
                 rightShifter.shift(Shifter.ShifterPosition.ARM);
+                shifting = true;
             }
 
             //these values will be overwritten if needed
@@ -76,15 +94,27 @@ public class Teleop extends OpMode {
             rightShifter.setPowerFloat();
 
             //tank drive if shifted to drive
-            if (leftShifter.getPosition() == Shifter.ShifterPosition.DRIVE
-                    && rightShifter.getPosition() == Shifter.ShifterPosition.DRIVE) {
+            if (leftShifter.getPosition() == Shifter.ShifterPosition.DRIVE && !shifting) {
                 leftShifter.setPower(scale(gamepad1.left_stick_y));
                 rightShifter.setPower(scale(gamepad1.right_stick_y));
+            }
+
+            //drive the arm intake motor
+            if (gamepad1.right_bumper || gamepad2.right_bumper) {
+                hardware.getArmIntakeMotor().setPower(-1);
+            } else if (gamepad1.right_trigger != 0 || gamepad2.right_trigger != 0) {
+                if (gamepad2.right_trigger != 0)
+                    hardware.getArmIntakeMotor().setPower(gamepad2.right_trigger);
+                else
+                    hardware.getArmIntakeMotor().setPower(gamepad1.right_trigger);
+            } else {
+                hardware.getArmIntakeMotor().setPower(0);
             }
 
         }//end if statement (!locked)
 
         //add sendTelemetryData data
         sendTelemetryData();
+
     }//end loop method
 }
