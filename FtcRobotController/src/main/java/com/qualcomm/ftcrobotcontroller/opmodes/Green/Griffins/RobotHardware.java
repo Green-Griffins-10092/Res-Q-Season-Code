d6423a500aab166361f317188c93d024c3576168
David <dflores0818@gmail.com>
2015-12-22 19:29:30 -0800

    Worked on the constructor for Robot Hardware.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Green/Griffins/RobotHardware.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Green/Griffins/RobotHardware.java
@@ -1,7 +1,7 @@
 package com.qualcomm.ftcrobotcontroller.opmodes.Green.Griffins;
 
+import com.qualcomm.hardware.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /**
@@ -11,7 +11,9 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  */
 public class RobotHardware {
 
-    public static final String[] MOTOR_NAMES = {};
+    public static final String[] MOTOR_NAMES = {"left arm", "right arm", "left drive", "right drive",
+                                                "telescope 1", "telescope 2", "intake", "turret"};
+    public static final String[] SENSOR_NAMES = {"turning gyro"};
 
     private DcMotor leftArmPivot, rightArmPivot;
     private DcMotor leftDriveMotor, rightDriveMotor;
@@ -19,10 +21,37 @@ public class RobotHardware {
     private DcMotor armIntakeMotor;
     private DcMotor turretPivotMotor;
 
-    private GyroSensor robotRotationGyro;
+    private ModernRoboticsI2cGyro robotRotationGyro;
 
     public RobotHardware(HardwareMap hardwareMap)
     {
+        //setting up arm pivot motors
+        leftArmPivot = hardwareMap.dcMotor.get(MOTOR_NAMES[0]);
+        rightArmPivot = hardwareMap.dcMotor.get(MOTOR_NAMES[1]);
+        leftArmPivot.setDirection(DcMotor.Direction.FORWARD);
+        rightArmPivot.setDirection(DcMotor.Direction.REVERSE);
 
+        //setting up drive motors
+        leftDriveMotor = hardwareMap.dcMotor.get(MOTOR_NAMES[2]);
+        rightDriveMotor = hardwareMap.dcMotor.get(MOTOR_NAMES[3]);
+        leftDriveMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightDriveMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        //setting up the telescope motors
+        armTelescopeMotor1 = hardwareMap.dcMotor.get(MOTOR_NAMES[4]);
+        armTelescopeMotor2 = hardwareMap.dcMotor.get(MOTOR_NAMES[5]);
+        armTelescopeMotor1.setDirection(DcMotor.Direction.FORWARD);
+        armTelescopeMotor2.setDirection(DcMotor.Direction.FORWARD);
+
+        //setting up the arm intake motor
+        armIntakeMotor = hardwareMap.dcMotor.get(MOTOR_NAMES[6]);
+        armIntakeMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        //setting up the turret pivot motor
+        turretPivotMotor = hardwareMap.dcMotor.get(MOTOR_NAMES[7]);
+        turretPivotMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        //setting up the gyro sensor
+        robotRotationGyro = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get(SENSOR_NAMES[0]);
     }
 }
