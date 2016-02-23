acc67d2a767d51c488fbadd8e2caa24a09e4abaa
David <dflores0818@gmail.com>
2016-02-22 17:45:28 -0800

    Made the class AutoFunctions, groups together the different functions for autonomous movement. Modified ClimberAuto and RampAuto to use this class.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Green/Griffins/ClimberAuto.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Green/Griffins/ClimberAuto.java
@@ -3,105 +3,25 @@ package com.qualcomm.ftcrobotcontroller.opmodes.Green.Griffins;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.util.RobotLog;
+
 
 /**
  * Created by David on 2/5/2016.
+ * The climber auto base class
  */
 
 public abstract class ClimberAuto extends LinearOpMode {
 
     protected static boolean blueSide = true;
     protected static int waitInMilliseconds = 0;
-    RobotHardware hardware;
-
-    public void twoWheelTurn(double angle) throws InterruptedException {
-
-        //curve around to face the ramp
-        int gyroTarget;
-        gyroTarget = hardware.getRobotRotationGyro().getIntegratedZValue() + (int) angle;
-
-        ElapsedTime timeout = new ElapsedTime();
-        double minimumPower = .15;
-        double drivePower = 1.0;
-        do {
-            waitForNextHardwareCycle();
-            int headingError = (gyroTarget - hardware.getRobotRotationGyro().getIntegratedZValue());
-            if (Math.abs(drivePower) < minimumPower) {
-                drivePower = minimumPower * Math.signum(drivePower);
-            }
-            hardware.getLeftDriveMotor().setPower(drivePower);
-            hardware.getRightDriveMotor().setPower(-drivePower);
-            drivePower = headingError / (2 * Math.abs(angle));
-            drivePower = Range.clip(drivePower, -1, 1);
-            RobotLog.i("2w -----------------------");
-            RobotLog.i("time: " + timeout.time());
-            RobotLog.i("error: " + headingError);
-            RobotLog.i("Motor power: " + drivePower);
-            telemetry.addData("error", headingError);
-            telemetry.addData("target, current", gyroTarget + ", " + hardware.getRobotRotationGyro().getIntegratedZValue());
-            telemetry.addData("Motor power", drivePower);
-        }
-        while (Math.abs(hardware.getRobotRotationGyro().getIntegratedZValue() - gyroTarget) > 0 && timeout.time() < 5);
-
-        //stop motors
-        waitForNextHardwareCycle();
-        hardware.getLeftDriveMotor().setPower(0);
-        hardware.getRightDriveMotor().setPower(0);
-
-        //send any late signals
-        waitForNextHardwareCycle();
-    }
-
-    public void driveStraight(long encoderCount, DriveStraightDirection direction, double power) throws InterruptedException {
-        if (power < 0) {
-            throw new IllegalArgumentException("Power must be greater than 0");
-        } else if (power > 1) {
-            throw new IllegalArgumentException("Power must be less than 1");
-        }
-        if (encoderCount < 0) {
-            throw new IllegalArgumentException(" Encoder count must be greater than 0");
-        }
-        ElapsedTime timeout = new ElapsedTime();
-        //encoder target
-        long encoderTarget;
-
-        if (direction == DriveStraightDirection.BACKWARD) {
-            power = -power;
-            encoderTarget = hardware.getLeftDriveMotor().getCurrentPosition() - encoderCount;
-        } else {
-            encoderTarget = hardware.getLeftDriveMotor().getCurrentPosition() + encoderCount;
-        }
-        //drive forward, clearing front of ramp
-        timeout.reset();
-        boolean stopCondition;
-        do {
-            waitForNextHardwareCycle();
-            hardware.getLeftDriveMotor().setPower(power);
-            hardware.getRightDriveMotor().setPower(power);
-
-            if (direction == DriveStraightDirection.BACKWARD) {
-                stopCondition = hardware.getLeftDriveMotor().getCurrentPosition() > encoderTarget;
-            } else {
-                stopCondition = hardware.getLeftDriveMotor().getCurrentPosition() < encoderTarget;
-            }
-        }
-        while (stopCondition && timeout.time() < 5);
-
-        //stop motors
-        waitForNextHardwareCycle();
-        hardware.getLeftDriveMotor().setPower(0);
-        hardware.getRightDriveMotor().setPower(0);
-
-        //send any late signals
-        waitForNextHardwareCycle();
-
-    }
+    protected RobotHardware hardware;
+    protected AutoFunctions autoFunctions;
 
     @Override
     public void runOpMode() throws InterruptedException {
         hardware = new RobotHardware(hardwareMap);
+        autoFunctions = new AutoFunctions(hardware, this);
+
         ElapsedTime timeout = new ElapsedTime();
 
         hardware.getRobotRotationGyro().calibrate();
@@ -153,73 +73,25 @@ public abstract class ClimberAuto extends LinearOpMode {
 
         int angle = 39;
         if (blueSide) {
-            twoWheelTurn(angle);
+            autoFunctions.twoWheelTurn(angle);
         } else {
-            twoWheelTurn(-angle);
+            autoFunctions.twoWheelTurn(-angle);
         }
 
         waitForNextHardwareCycle();
         sleep(1000);
         waitForNextHardwareCycle();
 
-        driveStraight(1700, DriveStraightDirection.FORWARD, .5);
+        autoFunctions.driveStraight(1700, AutoFunctions.DriveStraightDirection.FORWARD, .5);
 
         waitForNextHardwareCycle();
         sleep(1000);
         waitForNextHardwareCycle();
 
-        driveStraight(1700, DriveStraightDirection.BACKWARD, .5);
+        autoFunctions.driveStraight(1700, AutoFunctions.DriveStraightDirection.BACKWARD, .5);
 
         waitForNextHardwareCycle();
         sleep(1000);
         waitForNextHardwareCycle();
-
-        /*int firstOut = -3140;
-        hardware.getLeftDriveMotor().setTargetPosition(firstOut);
-        hardware.getRightDriveMotor().setTargetPosition(firstOut);
-        hardware.getLeftDriveMotor().setPower(-driveSpeed);
-        hardware.getRightDriveMotor().setPower(-driveSpeed);
-
-        while (hardware.getLeftDriveMotor().getCurrentPosition() > firstOut && hardware.getRightDriveMotor().getCurrentPosition() > firstOut);
-        {
-            waitForNextHardwareCycle();
-        }
-
-        hardware.getLeftDriveMotor().setPower(0);
-        hardware.getRightDriveMotor().setPower(0);
-
-        int firstTurnLeft = -2277;
-        int firstTurnRight = -3595;
-
-        hardware.getLeftDriveMotor().setTargetPosition(firstTurnLeft);
-        hardware.getRightDriveMotor().setTargetPosition(firstTurnRight);
-        hardware.getLeftDriveMotor().setPower(driveSpeed);
-        hardware.getRightDriveMotor().setPower(-driveSpeed);
-
-        while (hardware.getLeftDriveMotor().getCurrentPosition() < firstTurnLeft && hardware.getRightDriveMotor().getCurrentPosition() > firstTurnRight);
-        {
-            waitForNextHardwareCycle();
-        }
-
-        hardware.getLeftDriveMotor().setPower(0);
-        hardware.getRightDriveMotor().setPower(0);
-
-        int secondDriveLeft = -7961;
-        int secondDriveRight = -9279;
-
-        hardware.getLeftDriveMotor().setTargetPosition(secondDriveLeft);
-        hardware.getRightDriveMotor().setTargetPosition(secondDriveRight);
-        hardware.getLeftDriveMotor().setPower(-driveSpeed);
-        hardware.getRightDriveMotor().setPower(-driveSpeed);
-
-        while (hardware.getLeftDriveMotor().getCurrentPosition() > secondDriveLeft && hardware.getRightDriveMotor().getCurrentPosition() > secondDriveRight);
-        {
-            waitForNextHardwareCycle();
-        }
-
-        hardware.getLeftDriveMotor().setPower(0);
-        hardware.getRightDriveMotor().setPower(0);*/
     }
-
-    enum DriveStraightDirection {FORWARD, BACKWARD}
 }
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Green/Griffins/RampAuto.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Green/Griffins/RampAuto.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.util.RobotLog;
 
 /**
@@ -16,6 +15,7 @@ public abstract class RampAuto extends LinearOpMode {
     protected static boolean blueSide = true;
     protected static int waitInMilliseconds = 0;
     RobotHardware hardware;
+    AutoFunctions autoFunctions;
 
     public void oneWheelTimedTurn( DcMotor turningMotor, long milliseconds) throws InterruptedException {
         waitForNextHardwareCycle();
@@ -23,7 +23,7 @@ public abstract class RampAuto extends LinearOpMode {
 
         double power = 1.0;
         turningMotor.setPower(power);
-        sleep( milliseconds - ((long) (milliseconds*.1)));
+        sleep(milliseconds - ((long) (milliseconds * .1)));
 
         while( System.currentTimeMillis() - startTime < milliseconds )
         {
@@ -32,7 +32,7 @@ public abstract class RampAuto extends LinearOpMode {
             turningMotor.setPower( power );
             sleep( 50 );
         }
-        turningMotor.setPower( 0 );
+        turningMotor.setPower(0);
         //send any late signals
         waitForNextHardwareCycle();
     }
@@ -70,88 +70,12 @@ public abstract class RampAuto extends LinearOpMode {
 
     }
 
-    public void oneWheelTurn(DcMotor turningMotor, double angle) throws InterruptedException {
-        double minimumPower = .2;
-        int gyroTarget = hardware.getRobotRotationGyro().getIntegratedZValue() + (int) angle;
-
-        ElapsedTime timeout = new ElapsedTime();
-        double drivePower = 1.0;
-        do {
-            waitForNextHardwareCycle();
-            int headingError = (gyroTarget - hardware.getRobotRotationGyro().getIntegratedZValue());
-
-            RobotLog.i("time: " + timeout.time());
-            RobotLog.i("error: " + headingError);
-            RobotLog.i("Motor power: " + drivePower);
-            RobotLog.i("-----------------------");
-            turningMotor.setPower(drivePower);
-            drivePower = headingError / (2*angle);
-            drivePower = Range.clip(drivePower, -1, 1);
-            if (Math.abs(drivePower) < minimumPower) {
-                drivePower = minimumPower * Math.signum(drivePower);
-            }
-            telemetry.addData("error", headingError);
-            telemetry.addData("Motor power", drivePower);
-        }
-        while (Math.abs(hardware.getRobotRotationGyro().getIntegratedZValue() - gyroTarget) > 0 && timeout.time() < 5);
-
-        //stop driving
-        waitForNextHardwareCycle();
-        hardware.getLeftDriveMotor().setPower(0);
-        hardware.getRightDriveMotor().setPower(0);
-
-        //send any late signals
-        waitForNextHardwareCycle();
-
-    }
-
-    public void twoWheelTurn(double angle) throws InterruptedException {
-
-        //curve around to face the ramp
-        int gyroTarget;
-        gyroTarget = hardware.getRobotRotationGyro().getIntegratedZValue() + (int) angle;
-
-        ElapsedTime timeout = new ElapsedTime();
-        double minimumPower = .15;
-        double drivePower = 1.0;
-        do {
-            waitForNextHardwareCycle();
-            int headingError = (gyroTarget - hardware.getRobotRotationGyro().getIntegratedZValue());
-            if (Math.abs(drivePower) < minimumPower) {
-                drivePower = minimumPower * Math.signum(drivePower);
-            }
-            hardware.getLeftDriveMotor().setPower(drivePower);
-            hardware.getRightDriveMotor().setPower(-drivePower);
-            drivePower = headingError / (2*Math.abs(angle));
-            drivePower = Range.clip(drivePower, -1, 1);
-            RobotLog.i("2w -----------------------");
-            RobotLog.i("time: " + timeout.time());
-            RobotLog.i("error: " + headingError);
-            RobotLog.i("Motor power: " + drivePower);
-            telemetry.addData("error", headingError);
-            telemetry.addData("target, current", gyroTarget + ", " + hardware.getRobotRotationGyro().getIntegratedZValue());
-            telemetry.addData("Motor power", drivePower);
-        }
-        while (Math.abs(hardware.getRobotRotationGyro().getIntegratedZValue() - gyroTarget) > 0 && timeout.time() < 5);
-
-        //stop motors
-        waitForNextHardwareCycle();
-        hardware.getLeftDriveMotor().setPower(0);
-        hardware.getRightDriveMotor().setPower(0);
-
-        //send any late signals
-        waitForNextHardwareCycle();
-    }
-
     @Override
     public void runOpMode() throws InterruptedException {
         ElapsedTime timeout = new ElapsedTime();
 
-        telemetry.addData("Instructions 1", "Retract arm using teleop, after arm is in,\nrun the spool in reverse until there is a small amount of tension on the cable.");
-        telemetry.addData("Instructions 2", "Restart robot, using the button on the driver station, to reset encoders.");
-        telemetry.addData("Instructions 3", "Set up on field, on the outer edge of the tile next to the ramp.  Press init and set auto timer.\nWait for signal to start auto to press start.");
-
         hardware = new RobotHardware(hardwareMap);
+        autoFunctions = new AutoFunctions(hardware, this);
 
         hardware.getRobotRotationGyro().calibrate();
 
@@ -172,9 +96,9 @@ public abstract class RampAuto extends LinearOpMode {
         int angle = 39;
 
         if (blueSide) {
-            oneWheelTurn(hardware.getLeftDriveMotor(), angle);
+            autoFunctions.oneWheelTurn(hardware.getLeftDriveMotor(), angle);
         } else {
-            oneWheelTurn(hardware.getRightDriveMotor(), -angle);
+            autoFunctions.oneWheelTurn(hardware.getRightDriveMotor(), -angle);
         }
         sleep(1000);
 
@@ -222,9 +146,9 @@ public abstract class RampAuto extends LinearOpMode {
 
         double twoWheelTurnAngle = 85;
         if (blueSide) {
-            twoWheelTurn(twoWheelTurnAngle);
+            autoFunctions.twoWheelTurn(twoWheelTurnAngle);
         } else {
-            twoWheelTurn(-twoWheelTurnAngle);
+            autoFunctions.twoWheelTurn(-twoWheelTurnAngle);
         }
 
         sleep(1000);
